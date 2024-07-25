404e9bb854d11389e659b270d3a090000f2aa1ee
Mateusz Tokarski <mateusztokarski.88@gmail.com>
2024-07-25 13:34:00 +0200

    main.py Add lesson a4.10 'Enum - Zad 1'

--- a4_10_enum/shop/tax_calculator.py
+++
@@ -1,9 +0,0 @@
-class TaxCalculator:
-    @staticmethod
-    def calculate_tax(total_price, category_name):
-        if category_name == "Owoce i warzywa":
-            return round(total_price * 0.05, 2)
-        elif category_name == "Jedzenie":
-            return round(total_price * 0.08, 2)
-        else:
-            return round(total_price * 0.20, 2)
