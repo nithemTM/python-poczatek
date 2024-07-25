3e61759885cfd2de661c47513e1096bb24a65248
Mateusz Tokarski <mateusztokarski.88@gmail.com>
2024-07-25 13:38:26 +0200

    main.py Add lesson a4.12 'Enum - Zad 1'

---
+++ a4_12_enum/shop/apple.py
@@ -0,0 +1,13 @@
+# Zad 1
+from dataclasses import dataclass
+
+
+@dataclass
+class Apple:
+    species: str = "Gold"
+    size: int = 2
+    price: int = 0
+    total_price: int = 0
+
+    def total_price_f(self, weight):
+        self.total_price = weight * self.price
