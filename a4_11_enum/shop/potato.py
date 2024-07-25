6fad54a4f6ac01907f6f047e6b0a2bf467483890
Mateusz Tokarski <mateusztokarski.88@gmail.com>
2024-07-25 13:36:01 +0200

    main.py Add lesson a4.10 'Enum - Zad 1'

---
+++ a4_11_enum/shop/potato.py
@@ -0,0 +1,13 @@
+# Zad 1
+from dataclasses import dataclass
+
+
+@dataclass
+class Potato:
+    species: str = "Lord"
+    size: int = 1
+    price: int = 0
+    total_price: int = 0
+
+    def total_price_f(self, weight):
+        self.total_price = weight * self.price
